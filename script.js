99375c0bb10ae7714812aa20149dd4af15c0468a
Shahmir Hussain <shahmir.hussain@internative.net>
2021-12-08 21:19:52 +0500

    First Commit

---
+++ script.js
@@ -0,0 +1,33 @@
+
+
+$('[data-text]').on('keyup', function(){
+    $(this).attr('data-text', $(this).text());
+  });
+
+  $('.center').slick({
+    centerMode: true,
+    centerPadding: '60px',
+    slidesToShow: 3,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
+
+
